d740022654275c6c38f209e65355d63fafb44e22
Sreeroop <sreeroopravi@gmail.com>
2024-01-20 11:34:59 +0100

    bubble sort algorithm implementation

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -2,4 +2,34 @@
 // Created by Sreeroop on 20/01/2024.
 //
 
-#include "Bubble_Sort.h"
+#include <iostream>
+using namespace std;
+
+// Bubble Sort Implementation
+void bubbleSort(int *a, int n) {
+    bool swapped;
+    do{
+        swapped = false;
+        for (int i = 0; i < n-1; i++){
+            if (a[i+1] < a[i]){
+                int temp = a[i];
+                a[i] = a[i+1];
+                a[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    }while(swapped);
+}
+
+int main(){
+
+    int array[] = {5,3,4,6,7,8,1,9,5};
+
+    bubbleSort(array, 9);
+
+    for( int i: array ){
+        cout<<i<<" ";
+    }
+
+}
+
